0b82b04a032068c080d3328cdba4a91391b7ca1f
eeplusing <epluisng@gmail.com>
2018-10-30 22:52:18 +0800

    索引

---
+++ 索引.sql
@@ -0,0 +1,9 @@
+--Ë÷Òý
+
+CREATE TABLE customers(
+customer_id NUMBER(8,0) NOT NULL,
+join_date DATE NOT NULL,
+customer_status VARCHAR2(8) NOT NULL,
+customer_name VARCHAR2(20) NOT NULL,
+creditrating VARCHAR2(10)
+);
