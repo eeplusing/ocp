c34eb3b9fd7223e214b3f127c2d71ddbab9aee27
eeplusing <epluisng@gmail.com>
2018-11-12 22:55:34 +0800

    dml

---
+++ DML.sql
@@ -0,0 +1,18 @@
+--insert
+SELECT * FROM products;
+SELECT * FROM orders;
+SELECT * FROM order_items;
+
+SELECT prod_seq.nextval FROM dual;
+INSERT INTO products VALUES(prod_seq.nextval, '12c sql', 'ACTIVE', 60, SYSDATE, 20);
+INSERT INTO products VALUES(prod_seq.nextval, '12c sql all', 'ACTIVE', 100, SYSDATE, 40);
+
+SELECT * FROM customers;
+
+INSERT INTO customers(customer_id,join_date, customer_status, customer_name, creditrating)
+VALUES(cust_seq.nextval, SYSDATE, 'ACTIVE', '张三'， 'A');
+INSERT INTO customers(customer_id,join_date, customer_status, customer_name, creditrating)
+VALUES(cust_seq.nextval, SYSDATE, 'ACTIVE', '李四'， 'B');
+
+INSERT INTO orders(order_id, order_date, order_status, order_amount, customer_id) 
+VALUES(order_seq.nextval, SYSDATE, 'COMPLETE', 3, 3);
